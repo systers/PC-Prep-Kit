e33241834748a4552b14047d8515a3a406212e8a
shreyans29 <shreyansshrimal.ss@gmail.com>
2017-06-29 20:55:43 +0530

    Solved issues with indentation

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -5,35 +5,35 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { RegService } from '../services/reg.service';
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+    selector: 'app-register',
+    templateUrl: './register.component.html',
+    styleUrls: ['./register.component.scss']
 })
 
 
 export class RegisterComponent {
-  regForm: FormGroup;
+    regForm: FormGroup;
 
-  constructor(private _regService: RegService, private _router: Router, fb: FormBuilder) {
-    this.regForm = fb.group({
-        fname : [null, Validators.compose([Validators.required])],
-        lname : [null, Validators.compose([Validators.required])],
-        email : [null, Validators.compose([Validators.required, Validators.pattern('[^ @]*@[^ @]*')])],
-        password : [null, Validators.compose([Validators.required,
-          Validators.pattern('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$')])],
-        confirmpassword : [null, Validators.compose([Validators.required])]
-      });
-  }
+    constructor(private _regService: RegService, private _router: Router, fb: FormBuilder) {
+        this.regForm = fb.group({
+            fname : [null, Validators.compose([Validators.required])],
+            lname : [null, Validators.compose([Validators.required])],
+            email : [null, Validators.compose([Validators.required, Validators.pattern('[^ @]*@[^ @]*')])],
+            password : [null, Validators.compose([Validators.required,
+              Validators.pattern('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$')])],
+            confirmpassword : [null, Validators.compose([Validators.required])]
+        });
+    }
 
-  onSubmit= function(form: any){
-    this._regService.registerUser(form).subscribe((successful: boolean): void => {
-        if (successful) {
-            this._router.navigateByUrl('/login');
-        } else {
-            this.errorMessage = 'Registration Unsuccesful';
-        }
-    }, err => {
-        this.errorMessage = err.info;
-    });
-  }
+    onSubmit= function(form: any){
+        this._regService.registerUser(form).subscribe((successful: boolean): void => {
+            if (successful) {
+                this._router.navigateByUrl('/login');
+            } else {
+                this.errorMessage = 'Registration Unsuccesful';
+            }
+        }, err => {
+            this.errorMessage = err.info;
+        });
+    }
 }
