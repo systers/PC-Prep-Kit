5db6e69f48f3759ce22d3b48f3fff186043850cf
raxerz <rajath_krishna@yahoo.co.in>
2017-08-08 21:57:36 +0530

    Made indentation changes

--- src/app/malaria-101/malaria-101.component.spec.ts
+++ src/app/malaria-101/malaria-101.component.spec.ts
@@ -3,23 +3,23 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { Malaria101Component } from './malaria-101.component';
 
 describe('Malaria101Component', () => {
-  let component: Malaria101Component;
-  let fixture: ComponentFixture<Malaria101Component>;
+    let component: Malaria101Component;
+    let fixture: ComponentFixture<Malaria101Component>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ Malaria101Component ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ Malaria101Component ]
+        })
+        .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(Malaria101Component);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(Malaria101Component);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should be created', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- src/app/malaria-101/malaria-101.component.ts
+++ src/app/malaria-101/malaria-101.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-malaria-101',
-  templateUrl: './malaria-101.component.html',
-  styleUrls: ['./malaria-101.component.scss']
+    selector: 'app-malaria-101',
+    templateUrl: './malaria-101.component.html',
+    styleUrls: ['./malaria-101.component.scss']
 })
 export class Malaria101Component {
 }
